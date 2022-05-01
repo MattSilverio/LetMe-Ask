720f9e7abf0e26265f171e4b619f9f3023c1e0ad
Matheus Phillipo Silverio Silva <matheusphillipo@gmail.com>
2022-05-01 03:02:21 -0300

    Persistencia de dados do usuario nas paginas criadas

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
 import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
 
+import { Route,Routes, BrowserRouter } from 'react-router-dom'
+
+import { AuthContextProvider } from './contexts/AuthContexts';
+
 
 function App() {
-  return (
-    <>
-      <NewRoom/>
 
-    </>
+  return (
+    <BrowserRouter>   
+      <AuthContextProvider> 
+          <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/rooms/new" element={<NewRoom/>}/>
+          </Routes>  
+      </AuthContextProvider>    
+    </BrowserRouter>
   );
 }
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,6 @@
+import { useContext } from 'react'
+import { useNavigate } from 'react-router-dom'
+
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
 import googleIconImg from '../assets/images/google-icon.svg'
@@ -5,9 +8,23 @@ import googleIconImg from '../assets/images/google-icon.svg'
 import { Button } from '../components/Button'
 
 import '../styles/auth.scss'
+import { AuthContext } from '../contexts/AuthContexts'
+
 
 
 export function Home(){
+  const navigate = useNavigate();
+  const {user , signInWithGoogle} = useContext(AuthContext)
+  
+
+  async function handleCreateRoom(){
+    if(!user){
+      await signInWithGoogle()
+    }
+    
+    navigate('/rooms/new')
+  }
+
   return(
     <div id="page-auth">
       <aside>
@@ -18,7 +35,7 @@ export function Home(){
       <main>
         <div className='main-content'>
           <img src={logoImg} alt="Letmeask"/>
-          <button className='create-room'>
+          <button onClick={handleCreateRoom} className='create-room'>
             <img src={googleIconImg} alt="Logo do google" />
             Crie sua sala com o Google
           </button>
